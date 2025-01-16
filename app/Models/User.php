c4d09972b5e8a133b0aad5aa3e73d686c51ab2d0
Adam Wojcik <wojcikadam.03@gmail.com>
2025-01-16 17:00:24 +0100

    db v2

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -22,6 +23,8 @@ class User extends Authenticatable
         'email',
         'password',
         'role',
+        'country',
+        'description',
     ];
 
     /**
@@ -45,5 +48,24 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function created_tournaments()
+    {
+        return $this->hasMany(Tournament::class);
+    }
+
+    public function tournament_games()
+    {
+        return $this->hasMany(TournamentGame::class);
+    }
+
+    public function participated_tournaments() {
+        return $this->belongsToMany(Tournament::class, 'tournament_user');
+    }
+
+    public function game_teams()
+    {
+        return $this->belongsToMany(GameTeam::class, 'game_team_user');
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,10 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => fake()->randomElement(['user', 'player','referee' , 'admin']),
+            'country' => fake()->country(),
+            'description' => fake()->sentence(),
+            'avatar' => fake()->imageUrl(100, 100, 'people', true, 'Avatar'),
             'remember_token' => Str::random(10),
         ];
     }
